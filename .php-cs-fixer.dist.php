0aac1a44ef9e4515d5fa684767349024a8768589
jekuaitk <jekua@aarhus.dk>
2026-05-21 20:45:03 +0200

    Updated friendsofphp/php-cs-fixer recipe

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -5,6 +5,10 @@ use PhpCsFixer\Runner\Parallel\ParallelConfigFactory;
 $finder = (new PhpCsFixer\Finder())
     ->in(__DIR__)
     ->exclude('var')
+    ->notPath([
+        'config/bundles.php',
+        'config/reference.php',
+    ])
 ;
 
 return (new PhpCsFixer\Config())
